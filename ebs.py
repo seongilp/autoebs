3ad5932809d2f7495c6fbe7d196b8cfe69ad335a
Seongil Park <zihado@gmail.com>
2023-05-15 13:34:04 +0900

    Create ebs.py

---
+++ ebs.py
@@ -0,0 +1,17 @@
+import time
+import requests
+import wget
+import telegram
+import re
+import os
+import ftplib
+import json
+
+url = "https://5dang.ebs.co.kr/auschool/detail?courseId=BK0KAKC0000000014"
+page_source = requests.get(url).text
+url = "https://wstrotu.ebs.co.kr" + re.split('wstrotu.ebs.co.kr',page_source,re.S)[1].replace(";"," ").split("?")[0]
+wget.download(url)
+filename = url.split('/')[-1]
+token = $BOT_TELEGRAM_TOKEN
+bot = telegram.Bot(token=token)
+bot.send_audio(chat_id = '-1001442562593', audio=open(filename,'rb'))
