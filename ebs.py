b3dad7d0565cb775fe6d808a12104fe7fd1beb19
Seongil Park <zihado@gmail.com>
2023-05-15 13:49:40 +0900

    Update ebs.py

--- ebs.py
+++ ebs.py
@@ -2,7 +2,7 @@ url = "https://5dang.ebs.co.kr/auschool/detail?courseId=BK0KAKC0000000014"
 page_source = requests.get(url).text
 url = "https://wstrotu.ebs.co.kr" + re.split('wstrotu.ebs.co.kr',page_source,re.S)[1].replace(";"," ").split("?")[0]
 wget.download(url)
-filename = url.split('/')[-1]
+# filename = url.split('/')[-1]
 # os.get.env?
 token = $BOT_TELEGRAM_TOKEN
 bot = telegram.Bot(token=token)
