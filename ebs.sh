1e93d7e2762a6d4f03baa7d78b7ef716fd483198
Seongil Park <zihado@gmail.com>
2023-07-25 10:08:10 +0900

    Update ebs.sh

--- ebs.sh
+++ ebs.sh
@@ -4,5 +4,5 @@ PROGRAM_NAME=$1
 RECORD_MINS=$(($2 * 60))
 REC_DATE=`date +%Y%m%d`
 FILE_NAME=$PROGRAM_NAME"_"$REC_DATE.m4a
-ffmpeg -re -i "$RADIO_ADDR" -vn -acodec copy -t $RECORD_MINS "$FILE_NAME" 2>&1
+ffmpeg -re -i "$RADIO_ADDR" -vn -acodec copy -t $RECORD_MINS "$FILE_NAME" > /dev/null 2>&1
 curl -X POST -H "Content-Type:multipart/form-data" -F audio=@$FILE_NAME -F chat_id=$CHAT_ID "https://api.telegram.org/bot$TOKEN/sendAudio"
