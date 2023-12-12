a721e59e8d026df6449d2b433fece90bcb7be015
Seongil Park <zihado@gmail.com>
2023-12-12 21:34:10 +0900

    Create ebs_v4.py

---
+++ ebs_v4.py
@@ -0,0 +1,51 @@
+import requests,wget,re
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import telegram
+import asyncio
+import warnings
+from urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+warnings.filterwarnings('ignore', 'Unverified HTTPS request')
+warnings.filterwarnings("ignore", category=InsecureRequestWarning)
+
+params = {
+    'returnUrl': 'https://5dang.ebs.co.kr',
+    'login_uri': 'https://5dang.ebs.co.kr/sso/login',
+    'i': 'zihado4',
+    'c': 'INGlife2019@'
+}
+
+header = {
+    'Referer':'https://5dang.ebs.co.kr/login',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+}
+
+token = '1645085402:AAEkgvhpnuo7THuxBx8TAbi-ki3XBWC9qQQ'
+chat_id = '-1001442562593'
+bot = telegram.Bot(token=token)
+
+login_url = 'https://5dang.ebs.co.kr/login'
+
+url_list = [
+    'https://5dang.ebs.co.kr/auschool/sub/replay?prodId=235&lectId=60359619&pageNum=8&orderby=NEW&situ=&startDate=&endDate=&pdfOnly=',
+]
+
+#rec_url = 'https://5dang.ebs.co.kr/auschool/sub/replay?prodId=235&lectId=60344311&pageNum=9&orderby=NEW&situ=&startDate=&endDate=&pdfOnly='
+
+
+filename = ''
+
+with requests.Session() as s:
+    for url in url_list:
+        res = s.post(login_url,headers=header,data=params,verify=False)
+        res = s.get(url,headers=header,verify=False)
+        url = "https://wstrotu.ebs.co.kr" + re.split('wstrotu.ebs.co.kr',res.text,re.S)[1]+ "wstrotu.ebs.co.kr"
+        wget.download(url)
+        filename = url.split('?')[0].split('/')[-1]
+
+async def main():
+    await bot.send_audio(chat_id=chat_id, audio=open(filename,'rb'))
+
+asyncio.run(main())
+# if __name__ == '__main__':
+#     print_hi('PyCharm')±±
